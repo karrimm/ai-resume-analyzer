c0534dcccee653c29fa92a32f459dd96dcadb2f7
karrimm <karimbdfpro@gmail.com>
2025-09-14 19:12:29 +0100

    Cloud storage, PFD-image conversion, Key-value storage and AI Feedback

---
+++ app/routes/upload.tsx
@@ -0,0 +1,130 @@
+import {type FormEvent, useState} from "react";
+import Navbar from "~/components/Navbar";
+import {useNavigate, useParams} from "react-router";
+import FileUploader from "~/components/fileUploader";
+import {usePuterStore} from "../lib/puter";
+import {convertPdfToImage} from "~/lib/pdf2img";
+import {generateUUID} from "~/lib/utils";
+import {prepareInstructions} from "../../constants";
+
+const Upload = () => {
+
+    const { auth, isLoading, ai, fs, kv} = usePuterStore();
+    const navigate = useNavigate();
+    const [isProcessing, setIsProcessing] = useState(false)
+    const [StatusText, setStatusText] = useState('');
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileSelect = (file : File | null) => {
+        console.log("Selected file :", file)
+        setFile(file);
+    }
+
+    const handleAnalyze = async ( {companyName, jobTitle, jobDescription, file} : {companyName: string, jobTitle: string, jobDescription: string, file: File} ) => {
+        setIsProcessing(true);
+
+        setStatusText("Uploading the File ...");
+        const uploadedFile = await fs.upload([file]);
+        if (!uploadedFile) return setStatusText("Failed to upload file!");
+
+        setStatusText('Converting to image...');
+        const imageFile = await convertPdfToImage(file);
+        if(!imageFile.file) return setStatusText(`Failed to convert PDF to image! ${imageFile.error || ""}`);
+
+        setStatusText("Uploading the image...");
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if (!uploadedImage) return setStatusText("Failed to upload image!");
+
+        setStatusText("Preparing data ...");
+        const uuid = generateUUID();
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName, jobTitle, jobDescription,
+            feedback: ''
+        }
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText('Analyzing file...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({jobTitle, jobDescription})
+        )
+
+        if (!feedback)  return setStatusText('Error: Failed to analyze resume');
+
+        const feedbackText = typeof feedback.message.content === 'string'
+            ? feedback.message.content
+            : feedback.message.content[0].text
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Analysis completed, redirecting ...');
+        console.log(data)
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        if(!form) return;
+        const formData = new FormData(form);
+
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
+
+        if(!file) return;
+        handleAnalyze({companyName, jobTitle, jobDescription, file});
+    }
+
+    return ( <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Smart Feedback, Tailored for your Personal Resume</h1>
+                {
+                    isProcessing ? (
+                    <>
+                        <h2>{StatusText}</h2>
+                        <img src="/public/images/resume-scan.gif" className=""/>
+                    </>
+                    ) : (
+                        <h2>Drop your Resume for ATS Scoring</h2>
+                    )
+                }
+                {
+                    !isProcessing && (
+                        <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+                            <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                                <input type="text" name="company-name" placeholder="Company Name" id="company-name" />
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-title">Job Title</label>
+                                <input type="text" name="job-title" placeholder="Job Title" id="job-title" />
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-description">Job Description</label>
+                                <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description" />
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="uploader">Upload Resume</label>
+                                <FileUploader onFileSelect={handleFileSelect} />
+                            </div>
+
+                            <button className="primary-button" type="submit">
+                                Analyze Resume
+                            </button>
+                        </form>
+                    )
+                }
+            </div>
+        </section>
+        </main>
+    )
+}
+
+export default Upload;
